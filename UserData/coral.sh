e2d4bfd136d3042ac893e2a9290fd8a49cba5d95
Favenzio Calvo <fcalvo@fsu.edu>
2018-03-14 14:39:16 -0400

    Installed apache and mysql

--- UserData/coral.sh
+++ UserData/coral.sh
@@ -10,7 +10,6 @@ echo "CoralAdminEmail: ${CORAL_ADMIN_EMAIL}" >> /root/build-params.txt
 echo "CoralSiteName: ${CORAL_SITE_NAME}" >> /root/build-params.txt
 echo "CustomShScriptUrl: ${CUSTOM_SH_SCRIPT_URL}" >> /root/build-params.txt
 
-
 # Mount external devices 
 mkfs -t ext4 /dev/xvdb
 mkfs -t ext4 /dev/xvdc
@@ -27,11 +26,19 @@ cp -prT /tmp/home /home
 rm -rf /tmp/var
 rm -rf /tmp/home
 
-
 # Set timezone
 rm -f /etc/localtime
 ln -s /usr/share/zoneinfo/US/Eastern /etc/localtime
 
-
-# Run updates & installations
+# Run updates
 yum -y update > /root/updates.txt
+
+# Install Apache
+yum -y install httpd24 > /root/install-log.txt 2>&1
+service httpd start >> /root/install-log.txt 2>&1
+chkconfig httpd on >> /root/install-log.txt 2>&1
+
+# Install mysql
+yum -y install mysql-server >> /root/install-log.txt 2>&1
+chkconfig mysqld on >> /root/install-log.txt 2>&1
+service mysqld start >> /root/install-log.txt 2>&1
